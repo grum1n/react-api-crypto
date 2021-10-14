c56e87dc18a637e91685394fc5ff44cda22e3f5b
grum1n <grumin.el.pastas@gmail.com>
2021-10-14 10:02:13 +0300

    added axios and useEffect code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=20&page=1&sparkline=false
-
-
-
 function App() {
+  const [coins, setCoins] = useState([]);
   
+  useEffect(() => {
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=20&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+    }).catch(error => console.log(error))
+  }, []);
 
   return (
     <div className='App'>
