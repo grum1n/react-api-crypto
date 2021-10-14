2ba7c1909a768b071e27a72d17d88d71c9224fd1
grum1n <grumin.el.pastas@gmail.com>
2021-10-14 09:38:20 +0300

    added crypto api link from coingecko.com
    https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=20&page=1&sparkline=false

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 
+https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=20&page=1&sparkline=false
+
+
+
 function App() {
+  
+
   return (
     <div className='App'>
       <h1>Api</h1>
